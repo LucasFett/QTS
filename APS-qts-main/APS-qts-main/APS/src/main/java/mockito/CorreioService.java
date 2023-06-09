159fef4462b0b53ef15303abd6083062ef90dc67
Faculdade SENAI <fatec.senai@UO692M4092184.acad692.com.br>
2023-06-09 20:25:51 -0300

    qtstrabalho

---
+++ APS-qts-main/APS-qts-main/APS/src/main/java/mockito/CorreioService.java
@@ -0,0 +1,12 @@
+package mockito;
+
+public class CorreioService {
+	
+	private String endereco;
+	private String numero;
+	
+	public String buscaEndereco(String cep) {
+		return endereco + numero;
+	}
+	
+}
